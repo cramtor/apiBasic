43dbc71f786c8db3650049351511d5c4ff573db7
Alejandro Macia <alejandro.macia-sanchez-prestataire@laposte.fr>
2022-03-23 13:17:07 +0100

    service.yaml & monolog

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
+
     /**
      * @Route("/library/list", name="library_list")
      */
-    Public function list() {
+    Public function list(Request $request, LoggerInterface $logger) {
+
+        $title = $request->get('title','La fundación');
+        $logger->info('list action');
         $response = new JsonResponse();
         $response->setData([
             'succes' => true,
@@ -23,6 +30,10 @@ class LibraryController extends AbstractController
                 [
                     'id' => 2,
                     'title' => 'El millor dels mons'
+                ],
+                [
+                    'id' => 3,
+                    'title' => $title
                 ]
             ]
         ]);
