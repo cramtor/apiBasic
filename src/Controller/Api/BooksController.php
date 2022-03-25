afb3579f8963eac036c96d0dcb697f95226a4132
Alejandro Macia <alejandro.macia-sanchez-prestataire@laposte.fr>
2022-03-25 13:06:21 +0100

    Form and validation

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Api;
 
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Book;
+use App\Form\Type\BookFormType;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -15,5 +19,24 @@ class BooksController extends AbstractFOSRestController
     */
     public function getAction(BookRepository $bookRepo){
         return $bookRepo->findAll();
+    }
+
+    /**
+    * @Rest\Post(path="/books")
+    * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks= true)
+    */
+    public function postAction(
+        EntityManagerInterface $em,
+        Request $request
+    ) {
+        $book = new Book();
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($book);
+            $em->flush(); 
+            return $book;           
+        }
+        return $form;
     }
 }
