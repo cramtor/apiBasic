76d79c0a553716b3ca75648f612e2f8b8b931d02
Alejandro Macia <alejandro.macia-sanchez-prestataire@laposte.fr>
2022-03-25 14:59:43 +0100

    DTO and images(bug)

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -9,7 +9,9 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Book;
+use App\Form\Model\BookDto;
 use App\Form\Type\BookFormType;
+use League\Flysystem\FilesystemOperator;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -27,12 +29,21 @@ class BooksController extends AbstractFOSRestController
     */
     public function postAction(
         EntityManagerInterface $em,
+        FilesystemOperator $defaultStorage,
         Request $request
     ) {
-        $book = new Book();
-        $form = $this->createForm(BookFormType::class, $book);
+        $bookDto = new BookDto();
+        $form = $this->createForm(BookFormType::class, $bookDto);        
         $form->handleRequest($request);
+        printf('hola');
         if($form->isSubmitted() && $form->isValid()){
+            /*$extension = explode('/',mime_content_type($bookDto->base64Image))[1];
+            $data = explode('+',$bookDto->base64Image);
+            $fileName = sprintf('%s.%s', uniqid('book_', true), $extension);
+            $defaultStorage->write($fileName, base64_decode($data[1]));*/
+            $book = new Book();
+            //$book->setImage($fileName);
+            $book->setTitle($bookDto->title);
             $em->persist($book);
             $em->flush(); 
             return $book;           
--- src/Form/Type/BookFormType.php
+++ src/Form/Type/BookFormType.php
@@ -5,7 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\Book;
+use App\Form\Model\BookDto;
 
 class BookFormType extends AbstractType
 {
@@ -13,12 +13,13 @@ class BookFormType extends AbstractType
     {
         $builder
             ->add('title', TextType::class);
+            //->add('base64Image,',TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Book::class,
+            'data_class' => BookDto::class,
         ]);
     }
 
